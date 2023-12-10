bf48500702ea6068ad347df10ad674f1dfd2daae
el-sam <samurai.h@yahoo.com>
2023-12-10 09:38:54 +0800

    sturcuter on navbar accordingly

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,12 @@ import { name, city } from "../data/data.js";
 
 function Home() {
   // update the JSX being returned!
-  return <div>Home</div>;
+  return (
+    <div id="home">
+      Home
+    </div>
+
+  );
 }
 
 export default Home;
